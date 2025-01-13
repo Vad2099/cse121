b131666ddc5f10b88dde367932d6361b552b47b3
Vad2099 <valdemarlasnibat@hotmail.es>
2025-01-12 19:06:55 -0600

    some stuff added

--- sandbox/dom-example.js
+++ sandbox/dom-example.js
@@ -1,3 +1,20 @@
 const link = document.querySelector("a");
+const sect = document.querySelector("section");
+const para = document.createElement("p");
+const text = document.createTextNode(
+    "-the premier source for web development knowledge.",
+);
+const linkPara = document.querySelector("p");
+
 link.textContent = "Mozilla Developer Network";
 link.href = "https://developer.mozilla.org";
+para.textContent = "We hope you enjoyed the ride";
+sect.appendChild(para);
+linkPara.appendChild(text);
+
+para.style.color = "white";
+para.style.backgroundColor = "black";
+para.style.padding = "10px";
+para.style.width = "250px";
+para.style.textAlign = "center";
+
